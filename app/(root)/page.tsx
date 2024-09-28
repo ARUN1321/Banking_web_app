00752d1898fc83c0daeb289c7470f44643f3ec93
ARUN1321 <aruneducation62@gmail.com>
2024-09-28 13:58:39 +0530

    WB-4, Header component creation.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { HeaderBox } from "@/components/headerBox";
+import React from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const loggeIn = { firstName: "User" };
 
-export default page
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            title={"Welcome"}
+            type="greeting"
+            user={loggeIn?.firstName}
+            subtext={"Manage your banck activies efficiently here"}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default page;
